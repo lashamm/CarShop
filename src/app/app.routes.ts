ae9868b3df86ed7972ee5964dafefb8ae5fe895e
lasha <maisuradze.llasha@gmail.com>
2025-06-24 23:26:23 +0400

    routing done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,15 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { errorContext } from 'rxjs/internal/util/errorContext';
 import { ErrorComponent } from './error/error.component';
+import { ProductComponent } from './product/product.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: "home", pathMatch : "full"},
     {path: 'home', component : HomeComponent},
-    {path: '**', component : ErrorComponent}
+    {path: 'products', component : ProductComponent},
+    {path: 'contact', component : ContactComponent },
+    {path: 'about', component :  AboutComponent},
+    {path: '**', component : ErrorComponent},
 ];
